abc7b52de051b63009536724d080f3a02d054450
Keitaro Hallberg <keihallberg@gmail.com>
2022-11-18 14:11:44 -0800

    Set up UI for launch screen

--- app/src/main/java/com/example/blindr/MainActivity.kt
+++ app/src/main/java/com/example/blindr/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.blindr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun gotoLogin(view: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+//
+    public fun gotoSignUp(view: View) {
+    //TODO: FIX THIS DUMB SHIT ---> startActivity(Intent(this, SignUpActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/example/blindr/SignUpActivity.kt
@@ -0,0 +1,37 @@
+package com.example.blindr
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun registerUser(view: View) {
+
+        val email: String = findViewById<EditText>(R.id.email_edit_text).text.toString()
+        val password: String = findViewById<EditText>(R.id.password_edit_text).text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, HomeActivity::class.java))
+                } else {
+                    Toast.makeText(this, "An error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
